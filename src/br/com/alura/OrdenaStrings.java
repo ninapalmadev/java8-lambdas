e063467e8d92326d1d473abd03870f06bf7126e5
Nina Palma <ninapalmadev@gmail.com>
2022-09-28 12:34:15 -0300

    trtabalha com method reference

--- src/br/com/alura/OrdenaStrings.java
+++ src/br/com/alura/OrdenaStrings.java
@@ -14,29 +14,27 @@ public class OrdenaStrings {
 		palavras.add("brilha uma estrela");
 		palavras.add("hunters");
 
-		Comparator<String> comparador = new ComparadorPorTamanho();
-		// Collections.sort(palavras, comparador);
-		palavras.sort(comparador);
+//		palavras.sort((s1, s2) -> {
+//			if (s1.length() < s2.length())
+//				return -1;
+//			if (s1.length() > s2.length())
+//				return 1;
+//			return 0;
+//		});
+
+//		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		
+		palavras.sort(Comparator.comparing(s -> s.length()));
+		palavras.sort(Comparator.comparing(String::length));
+		
 		System.out.println(palavras);
 
 //		for (String p : palavras) {
 //			System.out.println(p);
 //		}
 
-		palavras.forEach(s -> System.out.println(s));
+		palavras.forEach(System.out::println);
+
+		
 	}
-}
-
-class ComparadorPorTamanho implements Comparator<String> {
-
-	@Override
-	public int compare(String s1, String s2) {
-
-		if (s1.length() < s2.length())
-			return -1;
-		if (s1.length() > s2.length())
-			return 1;
-		return 0;
-	}
-
 }
